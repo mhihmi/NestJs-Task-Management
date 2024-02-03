4fb92883a04936817a2ac9fc847ff4b4621c4676
mhihmi <mehdihilmi@gmail.com>
2024-02-03 02:06:37 +0100

    Service Usage in controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+}
+
+// Same As (without helloWorld Method)
+// @Controller('tasks')
+// export class TasksController {
+//   tasksService: TasksService;
+
+//   constructor(tasksService: TasksService) {
+//     this.tasksService = tasksService;
+//   }
+
+//   helloWorld() {
+//     this.tasksService.doSomething();
+//   }
+// }
