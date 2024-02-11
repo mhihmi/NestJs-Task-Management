bb7f36b4524e51dab59e37f3ea8c7af6c0c31e2a
mhihmi <mehdihilmi@gmail.com>
2024-02-11 16:39:08 +0100

    4.43 Set Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_DIALECT as 'postgres', // We'll create a real config service later
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
