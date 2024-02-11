ac31ffce13a50b79b0768cafcd948c5e0f27f448
mhihmi <mehdihilmi@gmail.com>
2024-02-11 23:02:10 +0100

    4.47 Refactoring to Use TypeORM and DB

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,6 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -18,59 +17,61 @@ import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  // http://localhost:3000/tasks
+  // Without TypeORM //
+  // // http://localhost:3000/tasks
+  // // @Get()
+  // // async getAllTasks(): Promise<Task[]> {
+  // //   return this.tasksService.getAllTasks();
+  // // }
+  // // adding filters from query:
   // @Get()
-  // async getAllTasks(): Promise<Task[]> {
-  //   return this.tasksService.getAllTasks();
+  // async getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+  //   if (Object.keys(filterDto).length) {
+  //     // if we have any filters defined, call taskService.getTasksWithFilters
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   } else {
+  //     // otherwise, just get all tasks
+  //     return this.tasksService.getAllTasks();
+  //   }
   // }
-  // adding filters from query:
-  @Get()
-  async getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
-    if (Object.keys(filterDto).length) {
-      // if we have any filters defined, call taskService.getTasksWithFilters
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      // otherwise, just get all tasks
-      return this.tasksService.getAllTasks();
-    }
-  }
 
-  // http://localhost:3000/tasks/j1g251b16f1vb16f
-  @Get(':id')
-  async getTaskById(@Param('id') id: string): Promise<Task> {
-    return this.tasksService.getTaskById(id);
-  }
+  // // http://localhost:3000/tasks/j1g251b16f1vb16f
+  // @Get(':id')
+  // async getTaskById(@Param('id') id: string): Promise<Task> {
+  //   return this.tasksService.getTaskById(id);
+  // }
 
-  // 1st method
+  // // 1st method
+  // // @Post()
+  // // createTask(@Body() body) {
+  // //   console.log('body', body);
+  // // }
+
+  // // 2nd method more strict on incoming datas
   // @Post()
-  // createTask(@Body() body) {
-  //   console.log('body', body);
+  // async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+  //   return this.tasksService.createTask(createTaskDto);
   // }
 
-  // 2nd method more strict on incoming datas
-  @Post()
-  async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.tasksService.createTask(createTaskDto);
-  }
+  // // http://localhost:3000/tasks/j1g251b16f1vb16f
+  // @Delete(':id')
+  // async deleteTask(@Param('id') id: string): Promise<void> {
+  //   return this.tasksService.deleteTask(id); // return is permitted here as if void
+  // }
 
-  // http://localhost:3000/tasks/j1g251b16f1vb16f
-  @Delete(':id')
-  async deleteTask(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteTask(id); // return is permitted here as if void
-  }
+  // @Patch(':id/status')
+  // async updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ): Promise<Task> {
+  //   // We can destructuring or pass the updateTaskStatusDto to the service too
+  //   const { status } = updateTaskStatusDto;
 
-  @Patch(':id/status')
-  async updateTaskStatus(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  ): Promise<Task> {
-    // We can destructuring or pass the updateTaskStatusDto to the service too
-    const { status } = updateTaskStatusDto;
-
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
 
+///////////////////////////////////////////////////////////////////////
 // @Controller('tasks')
 // export class TasksController {
 //   constructor(private tasksService: TasksService) {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,84 +1,71 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+// import { v4 as uuid } from 'uuid'; // Without TypeORM
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
-
-  async getAllTasks(): Promise<Task[]> {
-    return this.tasks;
-  }
-
-  async getTasksWithFilters(filterDto: GetTasksFilterDto): Promise<Task[]> {
-    // simpler way to do it, but we'll use ORM later..
-    const { status, search } = filterDto;
-
-    // define a temporary array to hold the result
-    let tasks = await this.getAllTasks();
-
-    // do something with status
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-
-    // do something with search
-    // if (search) {
-    //   tasks = tasks.filter((task) => {
-    //     if (task.title.includes(search) || task.description.includes(search)) {
-    //       return true;
-    //     }
-    //     return false;
-    //   });
-    // }
-    if (search) {
-      tasks = tasks.filter(
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-
-    // return final result
-    return tasks;
-  }
-
-  async getTaskById(id: string): Promise<Task> {
-    // try to get task
-    const found = this.tasks.find((task) => task.id === id);
-
-    // if not found, throw an error (404 not found)
-    if (!found) {
-      throw new NotFoundException(`Task with ID "${id}" not Found`);
-    }
-    // otherwise, return the found task
-    return found;
-  }
-
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-    return task;
-  }
-
-  async deleteTask(id: string): Promise<void> {
-    const found = await this.getTaskById(id); // for error handler
-
-    this.tasks = this.tasks.filter((task) => task.id !== found.id); // not best option but we'll use ORM later
-  }
-
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    return task;
-  }
+  // Without TypeORM //
+  // private tasks: Task[] = [];
+  // async getAllTasks(): Promise<Task[]> {
+  //   return this.tasks;
+  // }
+  // async getTasksWithFilters(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  //   // simpler way to do it, but we'll use ORM later..
+  //   const { status, search } = filterDto;
+  //   // define a temporary array to hold the result
+  //   let tasks = await this.getAllTasks();
+  //   // do something with status
+  //   if (status) {
+  //     tasks = tasks.filter((task) => task.status === status);
+  //   }
+  //   // do something with search
+  //   // if (search) {
+  //   //   tasks = tasks.filter((task) => {
+  //   //     if (task.title.includes(search) || task.description.includes(search)) {
+  //   //       return true;
+  //   //     }
+  //   //     return false;
+  //   //   });
+  //   // }
+  //   if (search) {
+  //     tasks = tasks.filter(
+  //       (task) =>
+  //         task.title.includes(search) || task.description.includes(search),
+  //     );
+  //   }
+  //   // return final result
+  //   return tasks;
+  // }
+  // async getTaskById(id: string): Promise<Task> {
+  //   // try to get task
+  //   const found = this.tasks.find((task) => task.id === id);
+  //   // if not found, throw an error (404 not found)
+  //   if (!found) {
+  //     throw new NotFoundException(`Task with ID "${id}" not Found`);
+  //   }
+  //   // otherwise, return the found task
+  //   return found;
+  // }
+  // async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
+  // async deleteTask(id: string): Promise<void> {
+  //   const found = await this.getTaskById(id); // for error handler
+  //   this.tasks = this.tasks.filter((task) => task.id !== found.id); // not best option but we'll use ORM later
+  // }
+  // async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
