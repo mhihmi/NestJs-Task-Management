a31501d22c1eaf7e4529d7a95f3409c1f4d14e8d
mhihmi <mehdihilmi@gmail.com>
2024-02-12 01:48:07 +0100

    4.55 Remove Repository File atm

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,11 +3,10 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
-import { TasksRepository } from './tasks.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService, TasksRepository],
+  providers: [TasksService],
 })
 export class TasksModule {}
