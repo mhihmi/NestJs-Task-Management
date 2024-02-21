afdcf75ab7a9aff7c31343f3c339893068cd2477
mhihmi <mehdihilmi@gmail.com>
2024-02-21 19:59:40 +0100

    5.65 Store JWT secret securely

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: 'topSecret24',
-      signOptions: {
-        expiresIn: 3600,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        global: true,
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
     }),
   ],
   providers: [AuthService],
