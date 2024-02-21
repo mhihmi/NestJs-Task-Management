33b2ca7bd289a339e1cfdca02e47c3ced000a7f4
mhihmi <mehdihilmi@gmail.com>
2024-02-21 19:50:02 +0100

    5.65 Add JWT on SignIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,29 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthCredentialsDto } from 'src/users/dto/auth-credentials.dto';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './dto/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersService.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ access_token: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.usersService.findOne(username);
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      return { access_token: await this.jwtService.signAsync(payload) };
     } else {
       throw new UnauthorizedException('Please check your login credentials');
     }
